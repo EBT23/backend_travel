1bf2e92cbd28edf9838a67cfdc3afca9166f5990
aderohmatmaulana98 <aderohmatmaulana77@gmail.com>
2023-03-12 12:02:55 +0700

    merge sourecode

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -8,6 +8,9 @@ use App\Models\Shuttle;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\Response;
+use App\Models\Kota;
+use App\Models\TempatAgen;
+use App\Models\User;
 
 
 class Admin extends Controller
@@ -140,5 +143,171 @@ class Admin extends Controller
             'message' => 'Persediaan tiket berhasil dibuat',
             'data' => $persediaan_tiket
         ], Response::HTTP_OK);
+    }
+    // Supir
+    public function supir()
+    {
+        $supir = DB::table('roles')
+            ->join('users', 'roles.id', '=', 'users.role_id')
+            ->select('users.*', 'roles.roles')
+            ->where('roles.id', '=', '3')
+            ->get();
+
+        return response()->json($supir);
+    }
+
+    public function tambah_supir(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required',
+            'no_hp' => 'required',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:6',
+        ], [
+            'password.min' => 'password minimal 6',
+            'email.unique' => 'email sudah digunakan',
+        ]);
+
+        $users = DB::table('users')->insert([
+            'nama' => $request->nama,
+            'no_hp' => $request->password,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'role_id' => 3,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'supir berhasil ditambahkan',
+            'data' => $users
+        ], Response::HTTP_OK);
+    }
+
+    public function update_supir(Request $request, $id)
+
+    {
+        $data = $request->only(
+            'nama',
+            'no_hp',
+            'email',
+            'password',
+            'role_id',
+        );
+
+
+        $supir = DB::table('users')
+            ->where('id', $id)
+            ->update([
+                'nama' => $request->nama,
+                'no_hp' => $request->no_hp,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+                'role_id' => $request->role_id,
+            ]);
+        return response()->json([
+            'success' => true,
+            'message' => 'supir berhasil ditambahkan',
+            'data' => $request->all()
+        ], Response::HTTP_OK);
+    }
+
+    public function delete_supir($id)
+    {
+        $supir = User::findOrFail($id);
+        $supir->delete();
+        return response()->json(null, 204);
+    }
+
+    // KOTA
+    public function kota()
+    {
+        $kota = Kota::all();
+        return response()->json([
+            'data' => $kota
+        ]);
+    }
+
+    public function tambah_kota(Request $request)
+    {
+        $validated = $request->validate([
+            'nama_kota' => 'required',
+        ]);
+
+        // simpan data ke database
+        $data = new Kota;
+        $data->nama_kota = $validated['nama_kota'];
+        $data->save();
+
+        // kirim response
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Data berhasil ditambah'
+        ]);
+    }
+
+    public function update_kota(Request $request, $id)
+    {
+        $kota = Kota::findOrFail($id);
+        $kota->update($request->all());
+        return response()->json([
+            'success' => true,
+            'message' => 'kota berhasil diupdate',
+            'data' => $kota, 200
+        ]);
+    }
+
+    public function delete_kota($id)
+    {
+        $kota = Kota::findOrFail($id);
+        $kota->delete();
+        return response()->json(null, 204);
+    }
+
+    // TEMPAT AGEN
+
+    public function tempat_agen()
+    {
+        $tmagen = TempatAgen::all();
+        return response()->json([
+            'data' => $tmagen
+        ]);
+    }
+
+    public function tambah_tempat_agen(Request $request)
+    {
+        $validated = $request->validate([
+            'kota_id' => 'required',
+            'tempat_agen' => 'required',
+        ]);
+
+        // simpan data ke database
+        $data = new TempatAgen();
+        $data->kota_id = $validated['kota_id'];
+        $data->tempat_agen = $validated['tempat_agen'];
+        $data->save();
+
+        // kirim response
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Data berhasil ditambah'
+        ]);
+    }
+
+    public function update_tempat_agen(Request $request, $id)
+    {
+        $tmagen = TempatAgen::findOrFail($id);
+        $tmagen->update($request->all());
+        return response()->json([
+            'success' => true,
+            'message' => 'kota berhasil diupdate',
+            'data' => $tmagen, 200
+        ]);
+    }
+
+    public function delete_tempat_agen($id)
+    {
+        $tmagen = TempatAgen::findOrFail($id);
+        $tmagen->delete();
+        return response()->json(null, 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -32,4 +32,20 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::delete('/delete_shuttle/{id}', [Admin::class, 'delete_shuttle']);
     Route::get('/persediaan_tiket', [Admin::class, 'persediaan_tiket']);
     Route::post('/tambah_persediaan_tiket', [Admin::class, 'tambah_persediaan_tiket']);
+
+    //SUPIR
+    Route::get('/supir', [Admin::class, 'supir']);
+    Route::post('/tambah_supir', [Admin::class, 'tambah_supir']);
+    Route::put('/update_supir/{id}', [Admin::class, 'update_supir']);
+    Route::delete('/delete_supir/{id}', [Admin::class, 'delete_supir']);
+
+    // KOTA
+    Route::post('/tambah_kota', [Admin::class, 'tambah_kota']);
+    Route::put('/update_kota/{id}', [Admin::class, 'update_kota']);
+    Route::delete('/delete_kota/{id}', [Admin::class, 'delete_kota']);
+
+    // TEMPAT AGEN
+    Route::post('/tambah_tempat_agen', [Admin::class, 'tambah_tempat_agen']);
+    Route::put('/update_tempat_agen/{id}', [Admin::class, 'update_tempat_agen']);
+    Route::delete('/delete_tempat_agen/{id}', [Admin::class, 'delete_tempat_agen']);
 });
